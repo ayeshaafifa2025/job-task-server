d2059338ca5da5c8137527d76bfeb2498a2c0792
ayeshaafifa2025 <ayeshaafifa2025@gmail.com>
2026-01-18 00:44:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(express.json());
+app.use(cors(
+));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wstr9pt.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+ const db = client.db('job_task_db');
+    const foodCollection = db.collection('foods');
+   app.get('/foods', async (req, res) => {
+    const cursor = foodCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+});
+app.get('/foods/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await foodCollection.findOne(query);
+    res.send(result);
+});
+    app.post('/foods',async(req,res)=>{
+    const food = req.body;
+    const result = await foodCollection.insertOne(food);
+    res.send(result)
+    }
+    )
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
